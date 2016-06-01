e38792a9d1fab8361672402e1fff328a43b3cefc
FlyZhang <zhangfei614@126.com>
2016-06-01 17:43:45 +0800

    Only left one problem in HashTable

--- src/tsinghua/cs/zhangfei/leetcode/FractionRecurrinDecimal.java
+++ src/tsinghua/cs/zhangfei/leetcode/FractionRecurrinDecimal.java
@@ -1,5 +1,8 @@
 package tsinghua.cs.zhangfei.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Fei Zhang on 2016/5/29.
  * Email:zhangfei614@126.com
@@ -8,13 +11,33 @@ package tsinghua.cs.zhangfei.leetcode;
 public class FractionRecurrinDecimal {
     public String fractionToDecimal(int numerator, int denominator) {
         if(denominator == 0) return null;
-        int a = numerator/denominator;
-        int b = numerator%denominator;
-        if(b == 0) return ""+a;
-        return null;
+        boolean flag = (numerator >= 0 && denominator > 0) || (numerator <= 0 && denominator < 0);
+
+        long n = numerator;
+        long d = denominator;
+        n = Math.abs(n);
+        d = Math.abs(d);
+
+        long a = n / d;
+        long b = n % d;
+        if(b == 0) return flag ? ""+a : "-"+a;
+        StringBuilder sb = new StringBuilder();
+        Map<Long,Integer> map = new HashMap<Long, Integer>();
+        while(b != 0){
+            b = b*10;
+            if(map.containsKey(b)){
+                sb.insert(map.get(b).intValue(),'(');
+                sb.append(')');
+                break;
+            }
+            sb.append(b/d);
+            map.put(b, sb.length() - 1);
+            b = b % d;
+        }
+        return flag ? a+"."+sb.toString() : "-"+a+"."+sb.toString();
     }
 
     public static void main(String[] args){
-        System.out.println(new FractionRecurrinDecimal().fractionToDecimal(1,2));
+        System.out.println(new FractionRecurrinDecimal().fractionToDecimal(-1,-2147483648));
     }
 }
---
+++ src/tsinghua/cs/zhangfei/leetcode/SudokuSolver.java
@@ -0,0 +1,61 @@
+package tsinghua.cs.zhangfei.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Fei Zhang on 2016/6/1.
+ * Email:zhangfei614@126.com
+ */
+
+public class SudokuSolver {
+    private final static int N = 9;
+    public void solveSudoku(char[][] board) {
+        List<int[]> emptyCells  = new LinkedList<int[]>();
+        boolean[][] row = new boolean[N][N];
+        boolean[][] col = new boolean[N][N];
+        boolean[][] sec = new boolean[N][N];
+
+        for(int i = 0; i < N; i++)
+            for(int j = 0; j < N; j++){
+                if(board[i][j]=='.'){
+                    emptyCells.add(new int[]{i,j});
+                }else{
+                    int idx = board[i][j]-'1';
+                    row[i][idx] = true;
+                    col[j][idx] = true;
+                    int k = i/3*3 + j/3;
+                    sec[k][idx] = true;
+                }
+            }
+        if (!emptyCells.isEmpty()){
+            subSloveSudoku(board,row,col,sec,emptyCells,0);
+        }
+    }
+
+    private boolean subSloveSudoku(char[][] board,boolean[][] row,boolean[][] col,boolean[][] sec,List<int[]> emptyCells,int idx){
+        if(idx == emptyCells.size()){
+            return true;
+        }
+        int[] cell = emptyCells.get(idx);
+        int x = cell[0];
+        int y = cell[1];
+        int k = x/3*3 + y/3;
+        for(int i = 0; i < N; i++){
+            if(!row[x][i] && !col[y][i] && !sec[k][i]){
+                board[x][y] = (char)('1'+i);
+                row[x][i] = true;
+                col[y][i] = true;
+                sec[k][i] = true;
+                if(subSloveSudoku(board,row,col,sec,emptyCells,idx+1)){
+                    return true;
+                }
+                board[x][y] = '.';
+                row[x][i] = false;
+                col[y][i] = false;
+                sec[k][i] = false;
+            }
+        }
+        return false;
+    }
+}
