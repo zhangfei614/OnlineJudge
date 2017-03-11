fce48f5a51649326e5d1a00e2edbe908346b0c7d
FlyZhang <zhangfei614@126.com>
2017-03-11 11:22:24 +0800

    two more spiralMatrix

--- src/tsinghua/cs/zhangfei/leetcode/SpiralMatrix.java
+++ src/tsinghua/cs/zhangfei/leetcode/SpiralMatrix.java
@@ -6,33 +6,43 @@ import java.util.List;
 /**
  * Created by Fei Zhang on 2016/4/7.
  * Email:zhangfei614@126.com
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+ * For example,
+ * Given the following matrix:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * You should return [1,2,3,6,9,8,7,4,5].
  */
 
 public class SpiralMatrix {
+
+    //使用四个变量分别控制边界,然后在四个方向上进行循环
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<Integer>();
-        if(matrix.length == 0 ) return list;
-
-        int M = matrix.length;
-        int N = matrix[0].length;
-        int m = M;
-        int n = N;
-        while(m > 0 && n > 0){
-            int bm = (M - m)/2;
-            int bn = (N - n)/2;
-            for(int i = 0; i < n && bm >= 0; i++) list.add(matrix[bm][bn+i]);
-            for(int i = 1; i < m-1 && N-bn-1>=0; i++) list.add(matrix[bm+i][N-bn-1]);
-            for(int i = 1; i < n-1 && M-bm-1>=0; i++) list.add(matrix[M-bm-1][N-bn-i-1]);
-            for(int i = 1; i < m-2 && bn >= 0; i++) list.add(matrix[M-bm-i-1][bn]);
-            m--;
-            n--;
+        if (matrix.length == 0) return list;
+        int M = matrix.length, N = matrix[0].length;
+        int top = 0, left = 0, right = 0, bottom = 0;
+        while (top + bottom < M && left + right < N) {
+            //向右走，直到遇到left边界
+            for (int i = left; i < N - right; i++) list.add(matrix[top][i]);
+            if (++top + bottom >= M) break;
+            //向下走，直到遇到buttom边界
+            for (int i = top; i < M - bottom; i++) list.add(matrix[i][N - right - 1]);
+            if (++right + left >= N) break;
+            for (int i = N - right - 1; i >= left; i--) list.add(matrix[M - bottom - 1][i]);
+            if (++bottom + top >= M) break;
+            for (int i = M - bottom - 1; i >= top; i--) list.add(matrix[i][left]);
+            if (++left + right >= N) break;
         }
         return list;
     }
 
-    public static void main(String[] args){
-        int[][] matrix = {{6,9,7}};
-        new SpiralMatrix().spiralOrder(matrix);
-
+    public static void main(String[] args) {
+        int[][] matrix = {{6, 9, 7},{2,3,4},{2,5,6}};
+        List<Integer> list = new SpiralMatrix().spiralOrder(matrix);
+        System.out.println(list.toString());
     }
 }
---
+++ src/tsinghua/cs/zhangfei/leetcode/SprialMatrixII.java
@@ -0,0 +1,41 @@
+package tsinghua.cs.zhangfei.leetcode;
+
+/**
+ * Created by Fei Zhang on 2017/3/11.
+ * Email:zhangfei614@126.com
+ * Given an integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ * For example,
+ * Given n = 3,
+ * You should return the following matrix:
+ * [
+ * [ 1, 2, 3 ],
+ * [ 8, 9, 4 ],
+ * [ 7, 6, 5 ]
+ * ]
+ */
+
+public class SprialMatrixII {
+    /**
+     * 思路：按照四个边界，依次填写每个数字，直到填写到n*n
+     * @param n
+     * @return
+     */
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int count = 0;
+        int top = 0, left = 0, right = 0, bottom = 0;
+        while (top + bottom < n && left + right < n) {
+            //向右走，直到遇到left边界
+            for (int i = left; i < n - right; i++) result[top][i] = ++count;
+            if (++top + bottom >= n) break;
+            //向下走，直到遇到bottom边界
+            for (int i = top; i < n - bottom; i++) result[i][n - right - 1] = ++count;
+            if (++right + left >= n) break;
+            for (int i = n - right - 1; i >= left; i--) result[n - bottom - 1][i] = ++count;
+            if (++bottom + top >= n) break;
+            for (int i = n - bottom - 1; i >= top; i--) result[i][left] = ++count;
+            if (++left + right >= n) break;
+        }
+        return result;
+    }
+}
